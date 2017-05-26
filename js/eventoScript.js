09650abb6e22e72d16ad3aa64c817b024b4780b0
andreagrance <andrea_salinasgrance@hotmail.com>
2017-05-26 16:19:07 -0400

    Proyecto de Eventos
    Pagina de practicas de Bootstrap y tambien del proyecto de la tesis

---
+++ js/eventoScript.js
@@ -0,0 +1,15 @@
+/* 
+<div class="container">
+  <div class="row">
+       <div class="col-sm-3"><h2>FANTASY  <small>  Eventos  </small> </h2></div>
+  <div class="col-sm-1">Inicio</div>
+  <div class="col-sm-2">Organizacion de Eventos</div>
+  <div class="col-sm-2">Fiestas tematicas</div>
+  <div class="col-sm-2">Organizacion de Bodas</div>
+  <div class="col-sm-1">Nosotros</div>
+  <div class="col-sm-1">Contactanos</div>
+
+  </div>
+</div>
+<button  type="button"  class=" dropdown btn btn-lg" ><a href="#" class="dropdown-toggle" data-toggle="dropdown">Organizacion de Eventos <span class="caret"></span></a></button>
+*/
